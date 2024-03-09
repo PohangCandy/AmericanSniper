6fda6f228b9213689c33b1cd99e603d0ba49d63c
sethman7 <131145377+sethman7@users.noreply.github.com>
2024-03-09 17:56:13 +0900

    fixing connecting err

---
+++ Source/ASPrototype/Pawn/ABMainUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ABMainUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASPROTOTYPE_API UABMainUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
